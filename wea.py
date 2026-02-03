1a2107c92448dd93ec7590c9aba345a42c975ace
tehern <adriankwok1112@gmail.com>
2026-02-03 12:43:40 -0800

    Remove infinite loop for time printing
    Removed continuous time printing and sleep loop.

--- wea.py
+++ wea.py
@@ -1,12 +1,1 @@
-import time
-from datetime import datetime
-from zoneinfo import ZoneInfo
-while True:
-    print(time.time())
-    time.sleep(1)
-    print(time.localtime())
-    print(time.gmtime())
-    print(datetime.now(ZoneInfo("Australia/Sydney")))
-    print(time.perf_counter())
-    print(time.process_time())
-    print(time.monotonic())
+
